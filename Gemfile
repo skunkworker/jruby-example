78388cd638dac3e1570a5782e6df786ab19a1ac7
John Bolliger <johnbolliger@gmail.com>
2026-05-26 15:16:46 -0700

    initial commit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source "read-only-mirror.rubygems.org"
+source 'https://rubygems.org'
 
 ruby "~> 3.1"
 
---
+++ example.rb
@@ -0,0 +1,9 @@
+# Set the path to your Gemfile if the script is in a subdirectory
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('Gemfile', __dir__)
+
+require 'bundler/setup' # Adjusts the $LOAD_PATH to include gems from Gemfile
+Bundler.require         # Optional: Automatically requires all gems in the Gemfile
+
+puts Psych::VERSION.to_s
+
+puts "done"
